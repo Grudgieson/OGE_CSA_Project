cfe8477bcdb9ba5aacda0223cb58ce90697a55b3
Grudgieson <grudgieson@gmail.com>
2025-03-30 18:35:45 -0500

    Dictionaries and Methods for Dashboard's Highlights of highest active User, Reader, and day. still working on the Average Unique visitors.

--- OGE_Reader_Project/obj/Debug/net8.0/OGE_Reader_Project.AssemblyInfo.cs
+++ OGE_Reader_Project/obj/Debug/net8.0/OGE_Reader_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGE_Reader_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feca517bfd39e04ff75212a3bd67dc21c62b0ac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed07fe1aae11e50fee48491462a0d76f1468b6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGE_Reader_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGE_Reader_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
